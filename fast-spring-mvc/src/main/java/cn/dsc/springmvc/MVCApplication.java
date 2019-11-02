90838739c3da6f250f15442c5812fe5e0a140452
dingshichen <dingshicheng@bizvane.cn>
2019-11-02 15:59:53 +0800

    过滤器

--- fast-spring-mvc/src/main/java/cn/dsc/springmvc/MVCApplication.java
+++ fast-spring-mvc/src/main/java/cn/dsc/springmvc/MVCApplication.java
@@ -2,11 +2,13 @@ package cn.dsc.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author dingShiChen
  * @since 2019/11/1
  */
+@ServletComponentScan("cn.dsc.springmvc.filter")
 @SpringBootApplication
 public class MVCApplication {
 
---
+++ fast-spring-mvc/src/main/java/cn/dsc/springmvc/filter/FirstFilter.java
@@ -0,0 +1,45 @@
+package cn.dsc.springmvc.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * Filter是Servlet规范定义的，由Servlet容器支持
+ * @author dingShiChen
+ * @since 2019/11/2
+ */
+@Slf4j
+@Order(0)
+@WebFilter(urlPatterns = "/*")	//不需要再添加@Compent注解
+public class FirstFilter implements Filter {
+
+	/**
+	 * Servlet容器回调此方法完成初始化
+	 * @param filterConfig
+	 * @throws ServletException
+	 */
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		log.info("FirstFilter 初始化成功");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		log.info("FirstFilter 过滤动作开始");
+		//执行过滤链的下一个环节
+		chain.doFilter(request, response);
+		log.info("FirstFilter 过滤动作结束");
+	}
+
+	/**
+	 * Servlet容器调用此方法停止过滤器的工作，此时必定过滤器没有在执行任务
+	 */
+	@Override
+	public void destroy() {
+		log.info("FirstFilter 销毁");
+	}
+}
