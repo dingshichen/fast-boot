7c72edb7798c4f190b8b1ffeb965ea0694aff439
dingshichen <dingshicheng@bizvane.cn>
2019-11-02 18:29:28 +0800

    session处理

--- fast-spring-mvc/src/main/java/cn/dsc/springmvc/control/SessionController.java
+++ fast-spring-mvc/src/main/java/cn/dsc/springmvc/control/SessionController.java
@@ -1,8 +1,43 @@
 package cn.dsc.springmvc.control;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
 /**
  * @author dingShiChen
  * @since 2019/11/2
  */
+@Slf4j
+@RestController
 public class SessionController {
+
+	@Autowired
+	private HttpSession httpSession;
+
+	@GetMapping("/createSession")
+	public String createSession(String k, String v){
+		this.httpSession.setAttribute(k, v);
+		return "success";
+	}
+
+	@GetMapping("/readSession")
+	public String readSession(String k){
+		return (String) this.httpSession.getAttribute(k);
+	}
+
+	@GetMapping("/deleteSession")
+	public String deleteSession(String k){
+		this.httpSession.removeAttribute(k);
+		return "success";
+	}
+
+	@GetMapping("/invalidateSession")
+	public String invalidateSession(){
+		this.httpSession.invalidate();
+		return "success";
+	}
 }
