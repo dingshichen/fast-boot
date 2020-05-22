7baa8e69594ed2d842554bbf29c64a72e516c036
dingshichen <dingshichen@uniondrug.com>
2020-05-22 12:07:51 +0800

    去掉 example 代码

--- fast-test/src/main/java/cn/dsc/fasttest/mapper/RoleMapper.java
+++ fast-test/src/main/java/cn/dsc/fasttest/mapper/RoleMapper.java
@@ -1,14 +1,8 @@
 package cn.dsc.fasttest.mapper;
 
 import cn.dsc.fasttest.model.Role;
-import cn.dsc.fasttest.model.RoleExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface RoleMapper {
-    long countByExample(RoleExample example);
-
-    int deleteByExample(RoleExample example);
 
     int deleteByPrimaryKey(Long id);
 
@@ -16,13 +10,7 @@ public interface RoleMapper {
 
     int insertSelective(Role record);
 
-    List<Role> selectByExample(RoleExample example);
-
     Role selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") Role record, @Param("example") RoleExample example);
-
-    int updateByExample(@Param("record") Role record, @Param("example") RoleExample example);
 
     int updateByPrimaryKeySelective(Role record);
 
