58a17230c049d74da31848d97950442198396058
dingshichen <dingshichen@uniondrug.com>
2020-09-07 22:03:37 +0800

    删除一个不需要的模块

--- fast-es/fast-high-level/src/main/java/cn/dsc/highlevel/conf/service/impl/ElasticSearchClientBeanImpl.java
+++ fast-es/fast-high-level/src/main/java/cn/dsc/highlevel/conf/service/impl/ElasticSearchClientBeanImpl.java
@@ -5,6 +5,7 @@ import cn.dsc.highlevel.conf.service.ElasticSearchClientBean;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 
 import java.io.IOException;
@@ -26,7 +27,9 @@ public class ElasticSearchClientBeanImpl implements ElasticSearchClientBean {
 
 	@Override
 	public void init() {
-		this.client = new RestHighLevelClient(RestClient.builder(new HttpHost(elasticSearchClientProperties.getHost(), elasticSearchClientProperties.getPort())));
+		HttpHost httpHost = new HttpHost(elasticSearchClientProperties.getHost(), elasticSearchClientProperties.getPort());
+		RestClientBuilder restClientBuilder = RestClient.builder(httpHost);
+		this.client = new RestHighLevelClient(restClientBuilder);
 		log.info("RestHighLevelClient build success");
 	}
 
